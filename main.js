c39555dea32a3230c28c0641d14ebf5741cd0311
brunasantana395 <bruna.delphino.santana@escola.pr.gov.br>
2025-10-22 15:09:30 -0300

    .

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ botoes[i].onclick = function() {
 for (let j = 0;j <botoes.lenght;j++) {
 botoes[j].classlist.remove("ativo");
 textos[j].classlist.add("ativo")
+const botoes =document.querySelectorAll(".botao");
+const textos =document.querySelectorAll(".aba-conteudo");
+for (let i=0;i<botoes.length;i++) {
+botoes[i].onclick=function(){
+for (letj=0;J<botoes.length;j++){
+botoes[j].classListremove("ativo");
+textos[j].classList.remove("ativo");
+}
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+}
+}
+const tempoObjetivo = new 
